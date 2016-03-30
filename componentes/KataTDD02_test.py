53d68754e14f68d91231d997b059b9b2c7c400db
Lina <lina.m8a@gmail.com>
2016-03-29 20:42:56 -0500

    Paso 2:  Test para probar registro de independiente (Rojo)

--- componentes/KataTDD02_test.py
+++ componentes/KataTDD02_test.py
@@ -12,3 +12,40 @@ class KataTDD02Test(TestCase):
     def test_title(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('Busco Ayuda', self.browser.title)
+
+    def test_register(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('linkRegistrarse')
+        link.click()
+
+        nombre = self.browser.find_element_by_id('nombre')
+        nombre.send_keys('Francisco')
+
+        apellidos = self.browser.find_element_by_id('apellidos')
+        apellidos.send_keys('Saenz')
+
+        self.browser.implicitly_wait(3)
+        self.browser.find_element_by_xpath("//select[@id='idServicio']/option[text()='Pintura']").click()
+
+        aniosExperiencia = self.browser.find_element_by_id('aniosExperiencia')
+        aniosExperiencia.send_keys('4')
+
+        telefono = self.browser.find_element_by_id('telefono')
+        telefono.send_keys('3111111')
+
+        correoElectronico = self.browser.find_element_by_id('correoElectronico')
+        correoElectronico.send_keys('pacho4@buscoayuda.com')
+
+        contrasenia = self.browser.find_element_by_id('contrasenia')
+        contrasenia.send_keys('clave1234')
+
+        foto = self.browser.find_element_by_id('foto')
+        foto.send_keys('http://es.fakenamegenerator.com/images/sil-male.png')
+
+        botonGuardar = self.browser.find_element_by_id('butGuardar')
+        botonGuardar.click()
+
+        self.browser.implicitly_wait(30)
+        p = self.browser.find_element_by_xpath("//p[text()='Francisco Saenz']")
+
+        self.assertIn('Francisco Saenz', p.text)
