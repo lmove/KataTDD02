6edba6cac07555c65137e262a32ad43d7bb1db6f
catherinevelandia <c.velandia10@uniandes.edu.co>
2016-03-29 20:35:08 -0500

    Paso 1:  Test para probar que el titulo de la página inicial sea “Busco Ayuda” (Rojo)

---
+++ componentes/KataTDD02_test.py
@@ -0,0 +1,14 @@
+from unittest import  TestCase
+from selenium import webdriver
+
+class KataTDD02Test(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Busco Ayuda', self.browser.title)
