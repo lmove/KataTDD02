00f989a34ae3d8f1e92e31261825af73c90d7590
catherinevelandia <c.velandia10@uniandes.edu.co>
2016-03-30 19:10:16 -0500

    Se actualiza la conexion a la BD con las variables de entorno

--- buscoayuda/settings.py
+++ buscoayuda/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'buscoayuda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'prueba',
-        'USER': 'postgres',
-        'PASSWORD': 'mati',
-        'HOST': 'localhost',
-        'PORT': '5432',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.environ['DATABASE_NAME'],
+       'USER': os.environ['DATABASE_USER'],
+       'PASSWORD': os.environ['DATABASE_PASSWORD'],
+       'HOST': os.environ['DATABASE_HOST'],
+       'PORT': os.environ['DATABASE_PORT'],
     }
 }
 
